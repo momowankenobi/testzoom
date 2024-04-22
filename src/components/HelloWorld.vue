8440a09b7b623d2cfca740de27507f267689cd22
Mohamed Wadih <wadihmomo1@gmail.com>
2024-04-22 18:24:01 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ZoomMtg } from '@zoom/meetingsdk';
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+
+var authEndpoint = 'http://localhost:4000'
+var sdkKey = 'abc123'
+var meetingNumber = '123456789'
+var passWord = ''
+var role = 0
+var userName = 'Vue.js'
+var userEmail = ''
+var registrantToken = ''
+var zakToken = ''
+var leaveUrl = 'http://localhost:5173'
+
+function getSignature() {
+  fetch(authEndpoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      meetingNumber: meetingNumber,
+      role: role
+    })
+  }).then((response) => {
+    return response.json()
+  }).then((data) => {
+    console.log(data)
+    startMeeting(data.signature)
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
+function startMeeting(signature) {
+  document.getElementById('zmmtg-root').style.display = 'block';
+
+  ZoomMtg.init({
+    leaveUrl: leaveUrl,
+    patchJsMedia: true,
+    success: (success) => {
+      console.log(success);
+      ZoomMtg.join({
+        signature: signature,
+        sdkKey: sdkKey,
+        meetingNumber: meetingNumber,
+        passWord: passWord,
+        userName: userName,
+        userEmail: userEmail,
+        tk: registrantToken,
+        zak: zakToken,
+        success: (success) => {
+          console.log(success);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    },
+    error: (error) => {
+      console.log(error);
+    }
+  });
+}
+</script>
+
+<template>
+  <h1>Zoom Meeting SDK Vue.js Sample</h1>
+
+  <button @click='getSignature'>Join Meeting</button>
+</template>
+
+<style scoped>
+
+</style>
